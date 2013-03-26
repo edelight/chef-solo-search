f4bd29964eccf9bbb8736753d19591aea9360b71
Maarten Hoogendoorn <maarten@moretea.nl>
2013-03-26 12:01:04 +0100

    Wrong test; we should always get a Chef::Node.

--- tests/test_search.rb
+++ tests/test_search.rb
@@ -196,7 +196,7 @@ module SearchNodeTests
     nodes = search(:node)
     assert_equal Chef::Node, nodes.find{ |n| n["hostname"] == "alpha.example.com" }.class
     assert_equal Chef::Node, nodes.find{ |n| n["hostname"] == "beta.example.com" }.class
-    assert_equal Hash, nodes.find{ |n| n["hostname"] == "wjc.example.com" }.class
+    assert_equal Chef::Node, nodes.find{ |n| n["hostname"] == "wjc.example.com" }.class
     assert_equal 3, nodes.length
   end
 
