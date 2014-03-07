cd58eee097d037c6e048a5a55184077e1699451f
charles-alan.alexander <charles-alan.alexander@hp.com>
2014-03-07 11:12:01 -0700

    Fixing the test.

--- tests/test_cookbook_structure.rb
+++ tests/test_cookbook_structure.rb
@@ -7,7 +7,7 @@ class TestNoFolder < Test::Unit::TestCase
   def test_no_folders_in_libraries
     libraries = File.expand_path('../../libraries/', __FILE__)
     results = Dir.entries(libraries).select {|f| File.directory? File.join(libraries, f)}
-    assert_equal results, [".", ".."]
+    assert_equal results.sort, [".", ".."]
   end
 
 end
